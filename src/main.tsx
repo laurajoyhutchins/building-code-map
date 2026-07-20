69c29b2b4afd389f9e1a71e66650ac71263c6a9f
Laura <laurajoyhutchins@gmail.com>
2026-07-20 01:47:23 -0600

    style: load jurisdiction resolver styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./styles.css";
+import "./resolution.css";
 
 const rootElement = document.getElementById("root");
 
